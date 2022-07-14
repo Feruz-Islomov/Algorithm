96b9eb9b7dfe39edc262bd0a5d8a7a97af3e6384
Abdulloh <firuzwest@gmail.com>
2022-07-14 03:39:22 +0300

    remove

--- customds.js
+++ customds.js
@@ -106,3 +106,133 @@ optimalqueue.dequeue();
 optimalqueue.enqueue("3y");
 optimalqueue.enqueue("4r");
 // console.log(optimalqueue.print());
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
+  }
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.prepend();
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
+    }
+  }
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+  print() {
+    let current = this.head;
+    let listValues = "";
+    while (current) {
+      listValues += `${current.value} `;
+      current = current.next;
+    }
+    console.log(listValues);
+  }
+}
+
+const linkedlist = new LinkedList();
+linkedlist.append(10);
+linkedlist.append(20);
+linkedlist.append(30);
+linkedlist.prepend(5);
+linkedlist.append(40);
+linkedlist.insert(333, 3);
+linkedlist.removeFrom(3);
+
+console.log(linkedlist.print());
+
+//empty
+// prepend(1){
+//   const node = {this.value = 1, this.next = null}
+//   this.head = {this.value = 1, this.next = null}
+// }
+
+// natijada {this.head = {this.value = 1, this.next = null}, this.size = 1 }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//not empty
+// prepend(2){
+//    const node = {this.value = 2, this.next = null}
+//    node.next =  {this.value = 1, this.next = null} this.head
+//    this.head =  {this.value = 2, this.next = null} node
+// }
+
+// cosnt list = {
+//   this.head = {
+//                this.value = 2,
+//                this.next = {
+//                             this.value = 1,
+//                             this.next = null
+//                            }
+//               },
+//   this.size = 2
+// }
